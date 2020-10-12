0f5377fcf70800dbd7328d03545a9779c6e15c50
Bowen Chen <chenbowen184@gmail.com>
2020-10-12 11:06:56 -0700

    add docker builds to app

--- disaster_messaging_app/app/controller.py
+++ disaster_messaging_app/app/controller.py
@@ -45,6 +45,25 @@ def version():
         return jsonify({"model_version": _version, "app_version": app_version})
 
 
+@classification_app.route("/v1/predict/model", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        # Step 1: Extract POST data from request body as JSON
+        json_data = request.get_json()
+        _logger.debug(f"Inputs: {json_data}")
+
+        # Step 3: Model prediction
+        response = make_prediction(input_data=json_data)
+        _logger.debug(f"Outputs: {response}")
+
+        # Step 4: Convert numpy ndarray to list
+        predictions = response.get("predictions")
+        version = response.get("version")
+
+        # Step 5: Return the response as JSON
+        return jsonify({"predictions": predictions, "version": version})
+
+
 @classification_app.route("/go")
 def go():
     # save user input in query
--- disaster_messaging_app/tests/test_controller.py
+++ disaster_messaging_app/tests/test_controller.py
@@ -7,7 +7,7 @@ from disaster_messaging_classification_model import __version__ as _version
 from disaster_messaging_classification_model.config import config as model_config
 from disaster_messaging_classification_model.utils.model_utils import load_data_from_db
 
-from app import __version__ as api_version
+from app import __version__ as app_version
 
 
 def test_health_endpoint_returns_200(flask_test_client):
@@ -26,7 +26,7 @@ def test_version_endpoint_returns_version(flask_test_client):
     assert response.status_code == 200
     response_json = json.loads(response.data)
     assert response_json["model_version"] == _version
-    assert response_json["api_version"] == api_version
+    assert response_json["app_version"] == app_version
 
 
 def test_prediction_endpoint_returns_prediction(flask_test_client):
@@ -39,19 +39,7 @@ def test_prediction_endpoint_returns_prediction(flask_test_client):
     post_json = X_test[0:1].to_json(orient="records")
 
     # When
-    response = flask_test_client.post("/go", json=json.loads(post_json))
+    response = flask_test_client.get("/go", json=json.loads(post_json))
 
     # Then
     assert response.status_code == 200
-    response_json = json.loads(response.data)
-    prediction = response_json["predictions"]
-    response_version = response_json["version"]
-    assert response_version == _version
-
-    assert isinstance(response.get("predictions")[0], dict)
-    assert len(response.get("predictions")[0]) == 36
-    assert response.get("predictions")[0]["related"] == 1
-
-    for i in categories:
-        if i != "related":
-            assert response.get("predictions")[0][i] == 0
