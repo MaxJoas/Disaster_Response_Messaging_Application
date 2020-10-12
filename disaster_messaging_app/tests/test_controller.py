3e370a509f1fa298fd2fbfb0eecec63d2260a4b9
Bowen Chen <chenbowen184@gmail.com>
2020-10-11 23:37:59 -0700

    fix circle error

--- disaster_messaging_app/tests/test_controller.py
+++ disaster_messaging_app/tests/test_controller.py
@@ -5,9 +5,9 @@ import os
 
 from disaster_messaging_classification_model import __version__ as _version
 from disaster_messaging_classification_model.config import config as model_config
-from disaster_messaging_classification_model.utils.utils import load_dataset
+from disaster_messaging_classification_model.utils.model_utils import load_data_from_db
 
-from api import __version__ as api_version
+from app import __version__ as api_version
 
 
 def test_health_endpoint_returns_200(flask_test_client):
@@ -35,16 +35,23 @@ def test_prediction_endpoint_returns_prediction(flask_test_client):
     # This is important as it makes it harder for the test
     # data versions to get confused by not spreading it
     # across packages.
-    test_data = load_dataset(file_name=model_config.TEST_DATA_FILE_NAME)
-    post_json = test_data[0:1].to_json(orient="records")
+    X_test, y_test, categories = load_data_from_db(set_label="test")
+    post_json = X_test[0:1].to_json(orient="records")
 
     # When
-    response = flask_test_client.post("/v1/predict/model", json=json.loads(post_json))
+    response = flask_test_client.post("/go", json=json.loads(post_json))
 
     # Then
     assert response.status_code == 200
     response_json = json.loads(response.data)
     prediction = response_json["predictions"]
     response_version = response_json["version"]
-    assert math.ceil(prediction[0]) == 531
     assert response_version == _version
+
+    assert isinstance(response.get("predictions")[0], dict)
+    assert len(response.get("predictions")[0]) == 36
+    assert response.get("predictions")[0]["related"] == 1
+
+    for i in categories:
+        if i != "related":
+            assert response.get("predictions")[0][i] == 0
